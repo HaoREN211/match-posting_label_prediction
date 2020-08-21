39bae98f545b665b4e1ee10c3b25ff63aa058945
REN Hao <renhaojules@163.com>
2020-08-21 18:36:55 +0800

    Word2Vec尝试

--- Main/m_tf_idf.py
+++ Main/m_tf_idf.py
@@ -8,6 +8,10 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import RidgeClassifier
 from sklearn.metrics import f1_score
 from Tools.tf_idf import find_best_tf_idf_vectorizer
+import numpy as np
+from sklearn.model_selection import KFold
+import pandas as pd
+import datetime as dt
 
 if __name__ == '__main__':
     # 获取训练集数据
@@ -17,9 +21,32 @@ if __name__ == '__main__':
     result = find_best_tf_idf_vectorizer(data_train, "text", min_df = [1, 10, 20], max_df=[1.0, 0.9, 0.8], ngram_min=[1],
         ngram_max=[1,2,3], norm=["l1", "l2", None], max_features=[None, 1000, 2000, 3000])
 
+    # 目前找到的最大的参数
+    # min_df = 1, max_df = 0.9, norm = "l2"
+    # ngram_min = 1, ngram_max=1
+
     tfidf = TfidfVectorizer(ngram_range=(1, 3), max_features=3000)
     train_test = tfidf.fit_transform(data_train['text'])
+    print(np.shape(train_test))
 
+    # 验证模型的好坏程度
+    n_validation, current_validation = 10, 1
+    folder = KFold(n_splits=n_validation, shuffle=True, random_state=0)
+    df_validation = pd.DataFrame(columns=["label", "predict"])
+    current_time = dt.datetime.now()
+    clf = RidgeClassifier()
+    for train_index, val_index in folder.split(train_test):
+        print("--- 当前完成" + str(round(current_validation/n_validation, 2)) + "，耗时" + str((dt.datetime.now() - current_time).seconds) + "秒。")
+        c_train_data, c_val_data = train_test[train_index], train_test[val_index]
+        clf.fit(c_train_data, data_train['label'].values[train_index])
+        df_validation = df_validation.append(pd.DataFrame({
+            "label": data_train['label'].values[val_index],
+            "predict": clf.predict(c_val_data)
+        }))
+        current_validation += 1
+    print("---模型准确率："+str(f1_score(list(df_validation["label"].values), list(df_validation["predict"].values), average="macro")))
+
+    # 单一验证
     clf = RidgeClassifier()
     clf.fit(train_test[:10000], data_train['label'].values[:10000])
 
--- Main/m_word2vec.py
+++ Main/m_word2vec.py
@@ -3,120 +3,128 @@
 # 时间：2020/8/20 16:45
 # IDE：PyCharm
 
-import logging
-import random
+# https://www.jianshu.com/p/6c8588d40d59
+# https://zhuanlan.zhihu.com/p/50243702
 
+import logging
 import numpy as np
-import torch
 import pandas as pd
 from gensim.models.word2vec import Word2Vec
+import pickle
+import os
+import datetime as dt
+from sklearn.linear_model import RidgeClassifier
+from sklearn.metrics import f1_score
+from sklearn.model_selection import KFold
+
+# 对于每一篇文章，获取文章的每一个分词在word2vec模型的相关性向量。
+# 然后把一篇文章的所有分词在word2vec模型中的相关性向量求和取平均数，即此篇文章在word2vec模型中的相关性向量。
+# 实例化Word2Vec对象时，关键字参数size定义为200，则相关性矩阵都为200维。
+# 定义getVector函数获取每个文章的词向量，传入2个参数，第1个参数是文章分词的结果，第2个参数是word2vec模型对象。
+def get_contentVector(cutWords, word2vec_model):
+    vector_list = [word2vec_model.wv[k] for k in cutWords if k in word2vec_model]
+    contentVector = np.array(vector_list).mean(axis=0)
+    return contentVector
+
+
+# 将数组转换成字符串
+def convert_array_to_string(list_array):
+    string_array = [str(x) for x in list_array]
+    return ",".join(string_array)
 
 if __name__ == '__main__':
-
-
+    # 20200821 尝试自己搭建word2vec模型
     logging.basicConfig(level=logging.INFO, format='%(asctime)-15s %(levelname)s: %(message)s')
+    model_path = "Model/model_word2vec.pickle"
+    vector_train_file, vector_test_file = "Data/Word2Vec/train.csv", "Data/Word2Vec/test.csv"
+    train_data = pd.read_csv('Data/train_set.csv', sep='\t', encoding='UTF-8')
+    test_data = pd.read_csv('Data/test_set.csv', sep='\t', encoding='UTF-8')
 
-    # set seed
-    seed = 666
-    random.seed(seed)
-    np.random.seed(seed)
-    torch.cuda.manual_seed(seed)
-    torch.manual_seed(seed)
+    # word2vec训练模型
+    num_features = 100  # Word vector dimensionality
+    num_workers = 8  # Number of threads to run in parallel
 
-    fold_num = 10
-    data_file = 'Data/train_set.csv'
+    # 提取text部分
+    list_text = [x.split() for x in train_data["text"].values]
+    list_text.extend([x.split() for x in test_data["text"].values])
 
+    # 如果之前保存了训练好了的模型，则从本地直接读取。否则重新训练模型。
+    if os.path.exists(model_path):
+        # 从本地读取模型
+        with open(model_path, 'rb') as file:
+            model = pickle.load(file)
+    else:
+        model = Word2Vec(list_text, workers=num_workers, size=num_features)
+        model.init_sims(replace=True)
 
+        # 保存模型到本地
+        with open(model_path, 'wb') as file:
+            pickle.dump(model, file)
 
-    def all_data2fold(fold_num, num=10000):
-        fold_data = []
-        f = pd.read_csv(data_file, sep='\t', encoding='UTF-8')
-        texts = f['text'].tolist()[:num]
-        labels = f['label'].tolist()[:num]
+    # 保存训练集词向量
+    if not (os.path.exists(vector_train_file) and os.path.exists(vector_test_file)):
+        # Word2Vec向量化
+        list_vector = [get_contentVector(x, model) for x in list_text]
 
-        total = len(labels)
+        # 保存训练集数据
+        df_train_vector = pd.DataFrame({
+            "label": train_data["label"].values,
+            "vector": list_vector[:len(train_data)]
+        })
 
-        index = list(range(total))
-        # 打乱index的顺序
-        np.random.shuffle(index)
+        # 保存测试集数据
+        df_test_vector = pd.DataFrame({
+            "vector": list_vector[len(train_data):]
+        })
 
-        all_texts = []
-        all_labels = []
-        for i in index:
-            all_texts.append(texts[i])
-            all_labels.append(labels[i])
+        # 将Word2Vec得到的num_features向量进行OneHot编码，向量中的一个元素成为编码中的一个特征(1列)
+        current_time = dt.datetime.now()
+        for i in range(num_features):
+            print("--- 当前第"+str(i)+"步，耗时"+str((dt.datetime.now()-current_time).seconds)+"秒。")
+            df_test_vector["c_" + str(i+1)] = df_test_vector.apply(lambda x: x["vector"][i], axis=1)
+            df_train_vector["c_" + str(i + 1)] = df_train_vector.apply(lambda x: x["vector"][i], axis=1)
+        # 删除向量列
+        df_train_vector.drop(columns=["vector"], inplace=True)
+        df_test_vector.drop(columns=["vector"], inplace=True)
+        df_train_vector.to_csv(vector_train_file, encoding="utf_8_sig", index=False)
+        df_test_vector.to_csv(vector_test_file, encoding="utf_8_sig", index=False)
 
-        # 转化为label-帖子ID内容列表对
-        label2id = {}
-        for i in range(total):
-            label = str(all_labels[i])
-            if label not in label2id:
-                label2id[label] = [i]
-            else:
-                label2id[label].append(i)
+    # 从文件中读取数据Word2Vec向量化之后的数据
+    df_train_vector = pd.read_csv(vector_train_file, encoding="utf_8_sig")
+    df_test_vector = pd.read_csv(vector_test_file, encoding="utf_8_sig")
 
-        # 各类型的帖子在每个batch中的数量一致
-        all_index = [[] for _ in range(fold_num)]
-        for label, data in label2id.items():
-            batch_size = int(len(data) / fold_num)
-            other = len(data) - batch_size * fold_num
-            for i in range(fold_num):
-                cur_batch_size = batch_size + 1 if i < other else batch_size
-                batch_data = [data[i * batch_size + b] for b in range(cur_batch_size)]
-                all_index[i].extend(batch_data)
+    # 验证模型的好坏程度
+    clf = RidgeClassifier()
+    n_validation, current_validation = 10, 1
+    folder = KFold(n_splits=n_validation, shuffle=True, random_state=0)
+    df_validation = pd.DataFrame(columns=["label", "predict"])
+    current_time = dt.datetime.now()
+    for train_index, val_index in folder.split(df_train_vector):
+        print("--- 当前完成" + str(round(current_validation/n_validation, 2)) + "，耗时" + str((dt.datetime.now() - current_time).seconds) + "秒。")
+        c_train_data, c_val_data = df_train_vector.loc[train_index, :], df_train_vector.loc[val_index, :]
+        clf.fit(c_train_data[df_test_vector.columns], c_train_data["label"].values)
+        df_validation = df_validation.append(pd.DataFrame({
+            "label": c_val_data["label"].values,
+            "predict": clf.predict(c_val_data[df_test_vector.columns])
+        }))
+        current_validation += 1
+    print("---模型准确率："+str(f1_score(list(df_validation["label"].values), list(df_validation["predict"].values), average="macro")))
 
-        batch_size = int(total / fold_num)
-        other_texts = []
-        other_labels = []
-        other_num = 0
-        start = 0
-        for fold in range(fold_num):
-            num = len(all_index[fold])
-            texts = [all_texts[i] for i in all_index[fold]]
-            labels = [all_labels[i] for i in all_index[fold]]
+    # save model
+    model.save("./word2vec.bin")
 
-            if num > batch_size:
-                fold_texts = texts[:batch_size]
-                other_texts.extend(texts[batch_size:])
-                fold_labels = labels[:batch_size]
-                other_labels.extend(labels[batch_size:])
-                other_num += num - batch_size
-            elif num < batch_size:
-                end = start + batch_size - num
-                fold_texts = texts + other_texts[start: end]
-                fold_labels = labels + other_labels[start: end]
-                start = end
-            else:
-                fold_texts = texts
-                fold_labels = labels
+    # load model
+    model = Word2Vec.load("./word2vec.bin")
 
-            assert batch_size == len(fold_labels)
+    # convert format
+    model.wv.save_word2vec_format('./word2vec.txt', binary=False)
 
-            # shuffle
-            index = list(range(batch_size))
-            np.random.shuffle(index)
+    # 调用Word2Vec模型对象的wv.most_similar方法查看与“2693”含义最相近的词。
+    model.wv.most_similar('2693')
 
-            shuffle_fold_texts = []
-            shuffle_fold_labels = []
-            for i in index:
-                shuffle_fold_texts.append(fold_texts[i])
-                shuffle_fold_labels.append(fold_labels[i])
+    # wv.most_similar方法使用positive和negative这2个关键字参数的简单示例。查看'2693' + '2024' - '4231'的结果，代码如下：
+    model.most_similar(positive=['2693', '2024'], negative=['4231'], topn=3)
 
-            data = {'label': shuffle_fold_labels, 'text': shuffle_fold_texts}
-            fold_data.append(data)
+    # 查看两个词的相关性，如所示：
+    model.similarity("2693", "2024")
 
-        logging.info("Fold lens %s", str([len(data['label']) for data in fold_data]))
-
-        return fold_data
-
-
-    fold_data = all_data2fold(10)
-
-    # 构建训练集
-    fold_id = 9
-    train_texts = []
-    for i in range(0, fold_id):
-        data = fold_data[i]
-        train_texts.extend(data['text'])
-
-    logging.info('Total %d docs.' % len(train_texts))
